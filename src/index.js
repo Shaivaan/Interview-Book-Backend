ecdff8deaf30a3aba187bdcd23186304f2d045ef
Shivanshu Mishra <93570605+Shaivaan@users.noreply.github.com>
2022-06-03 15:44:00 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 const connect  = require("./configs/db");
 const dataController = require("./controller/data.controller");
 const cors = require("cors");
-
+const port = process.env.PORT || 2333;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/data",dataController);
 
-app.listen(process.env.PORT || 2333,async ()=>{
+app.listen(port,async ()=>{
     try{
         await connect();
         console.log(process.env.PORT);
