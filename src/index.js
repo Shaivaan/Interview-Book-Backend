ac81b33a1865f0eec24ac3a3fba50645e8a16ea0
Shivanshu Mishra <93570605+Shaivaan@users.noreply.github.com>
2022-06-03 18:04:59 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const connect  = require("./configs/db");
 
-
 app.use(cors());
 app.use(express.json());
 
-
 const dataController = require("./controller/data.controller");
-require("dotenv").config();
-
 
 const port = process.env.PORT || 2333;
 app.use("/data",dataController);
+
 
 app.listen(port,async ()=>{
     try{
         await connect();
         console.log(port);
+        
     }catch(err){
         console.log(err);
     }
