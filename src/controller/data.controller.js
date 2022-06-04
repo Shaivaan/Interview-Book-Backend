3cfd4e7ad875196786b02f0e05e4945d709f54bc
Shivanshu Mishra <93570605+Shaivaan@users.noreply.github.com>
2022-06-04 13:03:17 +0530

    commit

--- src/controller/data.controller.js
+++ src/controller/data.controller.js
@@ -2,6 +2,16 @@ const express = require("express");
 const Data = require("../model/data.model");
 const router = express.Router();
 
+router.get("/",async(req,res)=>{
+    try{
+        const data = await Data.find().lean().exec();
+        res.send(data);
+    }catch(err){
+        console.log(err);
+    }
+})
+
+
 router.get("/:topic",async(req,res)=>{
     try{
         const data = await Data.find({topic:req.params.topic}).lean().exec();
@@ -11,14 +21,7 @@ router.get("/:topic",async(req,res)=>{
     }
 })
 
-router.get("/",async(req,res)=>{
-    try{
-        const data = await Data.find().lean().exec();
-        res.send(data);
-    }catch(err){
-        console.log(err);
-    }
-})
+
 
 
 module.exports = router;
